757ae434871c17bffe1c50882ff09771141af94a
ryusangwon <r8uuuuuuuu@gmail.com>
2023-08-11 20:39:07 +0900

    updates

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #SBATCH -J llama_lora_CNN_DM
-#SBATCH -o llama_lora_CNN_DM.%j.out
-#SBATCH -p 4A100
-#SBATCH --gres=gpu:6
+#SBATCH -o output/llama_lora_CNN_DM.%j.out
+#SBATCH -p A5000
+#SBATCH --gres=gpu:8
 #SBATCH -t 72:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
-conda activate llama
 python Llama_Lora_CNN_DM.py
